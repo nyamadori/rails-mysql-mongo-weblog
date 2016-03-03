2211faec004f09f06634813141bc83276b9d78b7
nyamadori <nyamadorig@gmail.com>
2016-03-04 05:52:44 +0900

    Add generator's configurations

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module RailsWeblog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.orm :active_record
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine false
+    end
   end
 end
