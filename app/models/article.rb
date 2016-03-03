7908aa8a827f7294f8328dfd3f0b3552912cac42
nyamadori <nyamadorig@gmail.com>
2016-03-04 07:58:41 +0900

    Implement #10

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ActiveRecord::Base
-  belongs_to :category
+  belongs_to :category, required: true
+  attr_accessor :title, :body
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :category, null: false
+  before_create :create_mongo_collections
+
+  private
+
+  def create_mongo_collections
+    self.title_oid = Content.create!(body: title, type: 'article_title').id
+    self.body_oid = Content.create!(body: body, type: 'article_body').id
+  end
 end
